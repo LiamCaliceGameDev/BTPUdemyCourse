91eb2b6a3d6f917c3a61cf093cd4d942373572e7
BTP <liamcalicegamedev@gmail.com>
2018-08-18 16:41:15 +0200

    Melee Enemy P1
    melee enemy part 1 script

--- UDEMYGameTutorial/Assets/Scripts/Enemy.cs
+++ UDEMYGameTutorial/Assets/Scripts/Enemy.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     public int health;
+    [HideInInspector]
+    public Transform player;
+
+    public float speed;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     public void TakeDamage (int amount) {
         health -= amount;
---
+++ UDEMYGameTutorial/Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : Enemy {
+
+    public float stopDistance;
+
+    private void Update()
+    {
+         
+        if(player != null) {
+
+            if (Vector2.Distance(transform.position, player.position) > stopDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+
+        }
+
+    }
+
+}
